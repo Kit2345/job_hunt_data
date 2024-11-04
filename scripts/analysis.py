3173e117de9400fabf2e1997618797d67fba87d7
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-11-04 13:17:28 +0000

    finished analysis and plots. Now have automated versus non autoamted and different tech tests

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -28,7 +28,7 @@ ax.set_xticks(year_applied_df["year_applied"])
 
 ax.set_ylabel('Number of applications')
 ax.set_xlabel('Year')
-ax.set_title('Number of applications by year')
+ax.set_title('Figure 1: Number of applications by year')
 
 
 # plt.show()
@@ -57,7 +57,7 @@ ax.set_xticks(month_applied_2024_df["month_applied"])
 
 ax.set_ylabel('Number of applications')
 ax.set_xlabel('Month')
-ax.set_title('Number of applications by month in 2024')
+ax.set_title('Figure 2: Number of applications by month in 2024')
 
 
 # plt.show()
@@ -73,7 +73,7 @@ colours = ["red", "green"]
 
 fig, ax = plt.subplots()
 ax.pie(received_reply_df["count"], labels=received_reply_df["received_reply"], autopct='%1.1f%%', startangle=90)
-ax.set_title("Have I received a reply from the job I applied for?")
+ax.set_title("Figure 3: Have I received a reply from the job I applied for?")
 # plt.show()
 
 
@@ -117,14 +117,7 @@ print("first interviews:")
 print(first_interviews_df)
 
 
-# # Automated first interview (% of first interviews)
-# rslt_df = df[df['first']  == "yes"]
-# automated_first_interview = rslt_df.groupby("automated first?").size()
-# automated_first_interview_df = automated_first_interview.reset_index(name="count")
-# # print(rslt_df[["number", "role", "first"]])
-# # print(automated_first_interview)
-# print("automated first interview?:")
-# print(automated_first_interview_df)
+
 
 
 
@@ -187,20 +180,21 @@ print(waiting_status)
 print(no_status)
 
 stage = (
+    "applied",
     "first_interview",
     "tech_test",
-    "final_interview",
+    
 )
 status = {
-    "yes": np.array(yes_status),
+    "passed": np.array(yes_status),
     "waiting": np.array(waiting_status),
-    "no": np.array(no_status),
+    "rejected": np.array(no_status),
 }
 
 colors = {
-    "yes": "tab:green",
+    "passed": "tab:green",
     "waiting": "tab:blue",
-    "no": "tab:red",
+    "rejected": "tab:red",
 }
 
 width = 0.5
@@ -224,7 +218,7 @@ for boolean, current_status in status.items():
 
     bottom += current_status
 
-ax.set_title("Application status")
+ax.set_title("Figure 4: Summary of the status of the jobs I applied for")
 ax.legend(loc="upper right")
 
 # plt.show()
@@ -250,9 +244,9 @@ waiting_status_percentage = percentage_df.loc[percentage_df['status'] == "waitin
 no_status_percentage = percentage_df.loc[percentage_df['status'] == "no", ["first_interview", "tech_test", "final_interview"]].values[0].tolist()
 
 status_percentage = {
-    "yes": np.array(yes_status_percentage),
+    "passed": np.array(yes_status_percentage),
     "waiting": np.array(waiting_status_percentage),
-    "no": np.array(no_status_percentage),
+    "rejected": np.array(no_status_percentage),
 }
 
 
@@ -268,22 +262,321 @@ for boolean, current_status in status_percentage.items():
         if value > 0:  # Only label if the value is greater than zero
             ax.text(p[i].get_x() + p[i].get_width() / 2,  # X position
                     bottom[i] + value / 2,  # Y position, centered in the bar
-                    value,  # Label from the dictionary
+                    f"{value}%",  # Label from the dictionary
                     ha='center', va='center', color='black', fontsize=8)  # Center the text and set color
 
     bottom += current_status
 
-ax.set_title("Application status percentage")
+ax.set_title("Figure 5: Summary of the status of the jobs I applied for")
+ax.legend(loc="upper right")
+
+# plt.show()
+
+
+
+# Automated first interview (% of first interviews)
+
+# Select for first interviews
+rslt_df = df[df['first']  == "yes"]
+# print(rslt_df[["first", "tech", "automated first?"]])
+
+# Separate automated and not automated into separate items
+automated_first_interview = rslt_df.loc[rslt_df["automated first?"] == "yes"]
+not_automated_first_interview = rslt_df.loc[rslt_df["automated first?"] == "no"]
+# print("automated")
+# print(automated_first_interview[["first", "tech", "automated first?"]])
+# print("not automated")
+# print(not_automated_first_interview[["first", "tech", "automated first?"]])
+
+# Summarise the automated and not automated applications
+automated_first_interview = automated_first_interview.groupby("tech").size()
+not_automated_first_interview = not_automated_first_interview.groupby("tech").size()
+# print("automated")
+# print(automated_first_interview)
+# print("not automated")
+# print(not_automated_first_interview)
+
+# Change the automated and not automated applications into dataframes
+automated_first_interview_df = automated_first_interview.reset_index(name="automated")
+automated_first_interview_df.rename(columns={'tech': 'status'}, inplace=True)
+# print(automated_first_interview_df)
+not_automated_first_interview_df = not_automated_first_interview.reset_index(name="not_automated")
+not_automated_first_interview_df.rename(columns={'tech': 'status'}, inplace=True)
+# print(not_automated_first_interview_df)
+
+# Merge the two summary  tables together
+automation_summary_df =pd.merge(automated_first_interview_df, not_automated_first_interview_df, on="status", how="outer").fillna(0)
+print("automation summary")
+print(automation_summary_df)
+
+# Stacked bar graph 
+
+# Re-order rows so it is yes waiting no
+automation_summary_df = automation_summary_df.reindex([2, 1, 0])
+print(automation_summary_df)
+
+# Getting each status into new python list 
+yes_status = automation_summary_df.loc[automation_summary_df['status'] == "yes", ["automated", "not_automated"]].values[0].tolist()
+waiting_status = automation_summary_df.loc[automation_summary_df['status'] == "waiting", ["automated", "not_automated"]].values[0].tolist()
+no_status = automation_summary_df.loc[automation_summary_df['status'] == "no", ["automated", "not_automated"]].values[0].tolist()
+print(yes_status)
+print(waiting_status)
+print(no_status)
+
+# Define variables to use for plot
+stage = (
+    "automated",
+    "not_automated",
+)
+
+status = {
+    "passed": np.array(yes_status),
+    "waiting": np.array(waiting_status),
+    "rejected": np.array(no_status),
+}
+
+colors = {
+    "passed": "tab:green",
+    "waiting": "tab:blue",
+    "rejected": "tab:red",
+}
+
+width = 0.5
+
+# Draw plot
+fig, ax = plt.subplots()
+bottom = np.zeros(2)
+
+for boolean, current_status in status.items():
+    p = ax.bar(stage, current_status, width, label=boolean, bottom=bottom, color = colors[boolean])
+    
+
+    # Add the labels centered in the bars
+    for i, value in enumerate(current_status):
+        if value > 0:  # Only label if the value is greater than zero
+            ax.text(p[i].get_x() + p[i].get_width() / 2,  # X position
+                    bottom[i] + value / 2,  # Y position, centered in the bar
+                    round(value),  # Label from the dictionary
+                    ha='center', va='center', color='black', fontsize=8)  # Center the text and set color
+
+    bottom += current_status
+
+ax.set_title("Figure 6: Comparing status of automated and not automated first interviews")
+ax.legend(loc="upper center")
+
+# plt.show()
+
+
+# Automation Percentage Stacked Bar Graph 
+
+# Getting totals 
+automation_totals = automation_summary_df[["automated", "not_automated"]].sum()
+print("totals")
+print(automation_totals)
+
+
+automation_percentage_df = automation_summary_df.copy()  
+automation_percentage_df[["automated", "not_automated"]] = (
+    automation_percentage_df[["automated", "not_automated"]].div(automation_totals) * 100
+).round(1)
+
+print(automation_percentage_df)
+
+# Getting status into python list
+yes_status_percentage = automation_percentage_df.loc[automation_percentage_df['status'] == "yes", ["automated", "not_automated"]].values[0].tolist()
+waiting_status_percentage = automation_percentage_df.loc[automation_percentage_df['status'] == "waiting", ["automated", "not_automated"]].values[0].tolist()
+no_status_percentage = automation_percentage_df.loc[automation_percentage_df['status'] == "no", ["automated", "not_automated"]].values[0].tolist()
+# print(yes_status_percentage)
+# print(waiting_status_percentage)
+# print(no_status_percentage)
+
+status_percentage = {
+    "passed": np.array(yes_status_percentage),
+    "waiting": np.array(waiting_status_percentage),
+    "rejected": np.array(no_status_percentage),
+}
+
+fig, ax = plt.subplots()
+bottom = np.zeros(2)
+
+for boolean, current_status in status_percentage.items():
+    p = ax.bar(stage, current_status, width, label=boolean, bottom=bottom, color = colors[boolean])
+    
+
+    # Add the labels centered in the bars
+    for i, value in enumerate(current_status):
+        if value > 0:  # Only label if the value is greater than zero
+            ax.text(p[i].get_x() + p[i].get_width() / 2,  # X position
+                    bottom[i] + value / 2,  # Y position, centered in the bar
+                    f"{value}%",  # Label from the dictionary
+                    ha='center', va='center', color='black', fontsize=8)  # Center the text and set color
+
+    bottom += current_status
+
+ax.set_title("Figure 7: Comparing status of automated and not automated first interviews")
+ax.legend(loc="upper center")
+
+# plt.show()
+
+
+
+# Types of tech tests
+
+# Separate types of tech tests
+rslt_df = df[df['tech']  == "yes"]
+algo = rslt_df[rslt_df["tech test type"] == "algorithmic problems"]
+pair_prog = rslt_df[rslt_df["tech test type"] == "Pair programming"]
+take_home = rslt_df[rslt_df["tech test type"] == "take-home"]
+# print(rslt_df["tech test type"])
+# print(algo[["first", "tech", "final", "tech test type"]])
+# print(pair_prog)
+# print(take_home)
+
+# Summarise status of different tech tests
+algo_status = algo.groupby("final").size()
+pair_prog_status =  pair_prog.groupby("final").size()
+take_home_status = take_home.groupby("final").size()
+# print(algo_status)
+# print(pair_prog_status)
+# print(take_home_status)
+
+# Change the tech test status into dataframes
+algo_status_df = algo_status.reset_index(name="algo")
+algo_status_df.rename(columns={'final': 'status'}, inplace=True)
+# print(algo_status_df)
+pair_prog_status_df = pair_prog_status.reset_index(name="pair_prog")
+pair_prog_status_df.rename(columns={'final': 'status'}, inplace=True)
+# print(pair_prog_status_df)
+take_home_status_df = take_home_status.reset_index(name="take_home")
+take_home_status_df.rename(columns={'final': 'status'}, inplace=True)
+# print(take_home_status_df)
+
+
+
+# # Merge the two summary  tables together
+tech_test_summary_df = pd.merge(algo_status_df, pair_prog_status_df, on="status", how="outer").fillna(0)
+# print(tech_test_summary_df)
+tech_test_summary_df = pd.merge(tech_test_summary_df, take_home_status_df, on="status", how="outer").fillna(0)
+# print(tech_test_summary_df)
+
+
+
+# Re-order rows so it is yes waiting no
+tech_test_summary_df = tech_test_summary_df.reindex([1, 0])
+print(tech_test_summary_df)
+
+# Getting each status into new python list 
+yes_status = tech_test_summary_df.loc[tech_test_summary_df['status'] == "yes", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+# waiting_status = tech_test_summary_df.loc[tech_test_summary_df['status'] == "waiting", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+no_status = tech_test_summary_df.loc[tech_test_summary_df['status'] == "no", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+# print(yes_status)
+# print(waiting_status)
+# print(no_status)
+
+# Define variables to use for plot
+stage = (
+    "algorithmic_problems",
+    "pair_progamming",
+    "take_home"
+)
+
+status = {
+    "passed": np.array(yes_status),
+    # "waiting": np.array(waiting_status),
+    "rejected": np.array(no_status),
+}
+
+colors = {
+    "passed": "tab:green",
+    # "waiting": "tab:blue",
+    "rejected": "tab:red",
+}
+
+width = 0.5
+
+# # Draw plot
+fig, ax = plt.subplots()
+bottom = np.zeros(3)
+
+for boolean, current_status in status.items():
+    p = ax.bar(stage, current_status, width, label=boolean, bottom=bottom, color = colors[boolean])
+    
+
+    # Add the labels centered in the bars
+    for i, value in enumerate(current_status):
+        if value > 0:  # Only label if the value is greater than zero
+            ax.text(p[i].get_x() + p[i].get_width() / 2,  # X position
+                    bottom[i] + value / 2,  # Y position, centered in the bar
+                    round(value),  # Label from the dictionary
+                    ha='center', va='center', color='black', fontsize=8)  # Center the text and set color
+
+    bottom += current_status
+
+ax.set_title("Figure 8: Comparing status of different types of tech tests")
+ax.legend(loc="upper right")
+# plt.show()
+
+# Tech test Percentage Stacked Bar Graph 
+
+# Getting totals 
+tech_test_totals = tech_test_summary_df[["algo", "pair_prog", "take_home"]].sum()
+print("totals")
+# print(tech_test_totals)
+
+
+tech_test_percentage_df = tech_test_summary_df.copy()  
+tech_test_percentage_df[["algo", "pair_prog", "take_home"]] = (
+    tech_test_percentage_df[["algo", "pair_prog", "take_home"]].div(tech_test_totals) * 100
+).round(1)
+
+# print(tech_test_percentage_df)
+
+# Getting status into python list
+yes_status_percentage = tech_test_percentage_df.loc[tech_test_percentage_df['status'] == "yes", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+# yes_status_percentage = tech_test_percentage_df.loc[tech_test_percentage_df['status'] == "waiting", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+no_status_percentage = tech_test_percentage_df.loc[tech_test_percentage_df['status'] == "no", ["algo", "pair_prog", "take_home"]].values[0].tolist()
+# print(yes_status_percentage)
+# print(waiting_status_percentage)
+# print(no_status_percentage)
+
+status_percentage = {
+    "passed": np.array(yes_status_percentage),
+    # "waiting": np.array(waiting_status_percentage),
+    "rejected": np.array(no_status_percentage),
+}
+
+fig, ax = plt.subplots()
+bottom = np.zeros(3)
+
+for boolean, current_status in status_percentage.items():
+    p = ax.bar(stage, current_status, width, label=boolean, bottom=bottom, color = colors[boolean])
+    
+
+    # Add the labels centered in the bars
+    for i, value in enumerate(current_status):
+        if value > 0:  # Only label if the value is greater than zero
+            ax.text(p[i].get_x() + p[i].get_width() / 2,  # X position
+                    bottom[i] + value / 2,  # Y position, centered in the bar
+                    f"{value}%",  # Label from the dictionary
+                    ha='center', va='center', color='black', fontsize=8)  # Center the text and set color
+
+    bottom += current_status
+
+ax.set_title("Figure 9: Comparing status of different types of tech test")
 ax.legend(loc="upper right")
 
 plt.show()
+
+
+
+
 
 # What do I want? 
 
 # 	- Job titles applied
 # 	- Job roles applied
 # 	- Types of tech test
-#     - Percentage of those that get to final interview
+
 
 
 
